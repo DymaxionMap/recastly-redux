2d930f66eec794044ac7d276af9cd7c706e2efbb
DymaxionMap <sthliao@gmail.com>
2018-11-28 20:53:58 -0800

    implement the redux for the nav bar

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
-import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+import handleVideoSearch from '../actions/search.js';
+var SearchContainer = () => {
+  
+};
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
 
-export default SearchContainer;
+var mapDispatchToProps = (dispatch) => ({
+  handleSearchInputChange: q => dispatch(handleVideoSearch(q)) });
+
+export default connect(null, mapDispatchToProps)(SearchContainer);
