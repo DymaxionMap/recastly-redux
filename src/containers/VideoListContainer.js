0a8510c18d9c767e78d9268d9b5d9437236fa67e
DymaxionMap <sthliao@gmail.com>
2018-11-28 21:19:28 -0800

    implement the redux for the video list

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -1,10 +1,17 @@
 import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
-import changeVideo from './../actions/currentVideo.js';
+import changeVideoList from './../actions/currentVideo.js';
 
 var VideoListContainer = () => {};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
-export default VideoListContainer;
+var mapDispatchToProps = (dispatch) => ({
+  handleVideoListEntryTitleClick: videos => dispatch(changeVideoList(videos)) });
+  
+var mapStateToProps = (state) => {
+  return { videos: state.videoList };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideoListContainer);
