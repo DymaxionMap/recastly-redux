e8c96971130c1903955d3aed85ad38238b3369d2
DymaxionMap <sthliao@gmail.com>
2018-11-28 19:10:29 -0800

    implement store

--- src/store/store.js
+++ src/store/store.js
@@ -3,3 +3,19 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+// createStore arguments:
+// 1. root reducer
+// 2. initial state
+// 3. enhancers (enhance the store with third-party capabilities such as 
+//    middleware, time travel, persistence, etc.)
+ 
+var store = createStore(
+  rootReducer,
+  {
+    currentVideo: null,
+    videoList: []
+  },
+  applyMiddleware(thunk)
+);
+
+export default store;
